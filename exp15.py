75143863120b4f9f6b9d8176abcd53d35dd10081
Vigneshvar16 <192424187.simats@saveetha.com>
2026-07-16 16:57:58 +0530

    Delete exp15.py

--- exp15.py
+++
@@ -1,9 +0,0 @@
-temps = []
-
-for i in range(7):
-    t = float(input(f"Day {i+1}: "))
-    temps.append(t)
-
-print("Maximum:", max(temps))
-print("Minimum:", min(temps))
-print("Average:", sum(temps) / 7)

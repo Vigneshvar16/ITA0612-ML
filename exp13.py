46b55b7d1d4df3db49a2d4b3cdfdc3cfb064b8ee
Vigneshvar16 <192424187.simats@saveetha.com>
2026-07-16 16:57:35 +0530

    Delete exp13.py

--- exp13.py
+++
@@ -1,9 +0,0 @@
-vehicle = input("Vehicle (bike/car): ")
-hours = int(input("Hours: "))
-
-if vehicle.lower() == "bike":
-    fee = hours * 20
-else:
-    fee = hours * 50
-
-print("Parking Fee:", fee)

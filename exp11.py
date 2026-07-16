60c8a44cd7711336263914dac3a1a9f0e3049e3e
Vigneshvar16 <192424187.simats@saveetha.com>
2026-07-16 16:57:15 +0530

    Delete exp11.py

--- exp11.py
+++
@@ -1,8 +0,0 @@
-distance = float(input("Distance (km): "))
-mileage = float(input("Mileage (km/l): "))
-price = float(input("Fuel Price/litre: "))
-
-litres = distance / mileage
-cost = litres * price
-
-print("Fuel Cost:", cost)
